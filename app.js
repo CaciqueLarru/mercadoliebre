1038abac8b1b5d6d89756020546f09a30a9d268d
nombreusuario <tuemail>
2021-09-28 08:46:46 +0900

    config para heroku

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const puerto=3000
 
 app.use(express.static(__dirname + '/views'));
 
+/* En caso de NO utilizar Heroku, usar esto
 app.listen(puerto, () => {
     console.log(`Servidor corriendo en puerto 3000`)
   });
+*/
+
+
+/* En caso de SI utilizar Heroku, usar esto */
+  app.listen(process.env.PORT || 3000,function () {
+    console.log(`Servidor corriendo en puerto 3000`)
+  });
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './views/home.html'));
